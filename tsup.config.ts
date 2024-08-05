d7f512659287eeb1b6e14cbb37633fdf96f0f6dd
Ricardo Prado <ricardo.prado@simpli.com.br>
2024-08-05 16:14:14 -0300

    Lib compatbility

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from "tsup";
+// tsup.config.ts
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    entry: ["src/index.ts"],
-    format: ["cjs", "esm"],
-    dts: true,
-    splitting: false,
-    sourcemap: true,
-    clean: true,
+  entry: ['src/index.ts'], 
+  format: ['cjs', 'esm'], 
+  dts: true, 
+  outDir: 'dist', 
+  sourcemap: true, 
+  splitting: false, 
+  clean: true, 
+  minify: false, 
+  esbuildOptions: (options) => {
+    options.outExtension = {
+      '.js': options.format === 'esm' ? '.mjs' : '.cjs',
+    };
+  },
 });
